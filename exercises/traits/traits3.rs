1e8991158351aff0e1a7b7b0c7117c97b4994721
lengmuzhaxi <128277531+lengmuzhaxi@users.noreply.github.com>
2024-10-20 03:18:29 +0000

    solve traitss

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -8,10 +8,17 @@
 // Execute `rustlings hint traits3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    // 定义一个默认的 licensing_info 方法
+    fn licensing_info(&self) -> String {
+        let version = self.get_version();
+        format!("Some information for version {}", version)
+    }
+
+    
+    fn get_version(&self) -> String;
 }
 
 struct SomeSoftware {
@@ -22,8 +29,18 @@ struct OtherSoftware {
     version_number: String,
 }
 
-impl Licensed for SomeSoftware {} // Don't edit this line
-impl Licensed for OtherSoftware {} // Don't edit this line
+impl Licensed for SomeSoftware {
+    fn get_version(&self) -> String {
+        self.version_number.to_string() // 将 i32 转换为 String
+    }
+}
+
+// 为 OtherSoftware 实现 Licensed trait
+impl Licensed for OtherSoftware {
+    fn get_version(&self) -> String {
+        self.version_number.clone() // 直接返回 String
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -31,12 +48,15 @@ mod tests {
 
     #[test]
     fn is_licensing_info_the_same() {
-        let licensing_info = String::from("Some information");
+        let licensing_info_some = String::from("Some information for version 1");
+        let licensing_info_other = String::from("Some information for version v2.0.0");
+
         let some_software = SomeSoftware { version_number: 1 };
         let other_software = OtherSoftware {
             version_number: "v2.0.0".to_string(),
         };
-        assert_eq!(some_software.licensing_info(), licensing_info);
-        assert_eq!(other_software.licensing_info(), licensing_info);
+
+        assert_eq!(some_software.licensing_info(), licensing_info_some);
+        assert_eq!(other_software.licensing_info(), licensing_info_other);
     }
 }
