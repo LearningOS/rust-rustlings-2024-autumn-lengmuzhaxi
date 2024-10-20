7add0c91e6deff0034066c5b70ff02d00988c0fa
lengmuzhaxi <128277531+lengmuzhaxi@users.noreply.github.com>
2024-10-20 02:24:24 +0000

    solve options2

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 #[cfg(test)]
 mod tests {
@@ -11,9 +11,7 @@ mod tests {
     fn simple_option() {
         let target = "rustlings";
         let optional_target = Some(target);
-
-        // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -28,15 +26,11 @@ mod tests {
         }
 
         let mut cursor = range;
-
-        // TODO: make this a while let statement - remember that vector.pop also
-        // adds another layer of Option<T>. You can stack `Option<T>`s into
-        // while let and if let.
-        integer = optional_integers.pop() {
+         // 使用 while let 循环，当从向量中 pop 出的值是 Some 时继续循环
+         while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
-
         assert_eq!(cursor, 0);
     }
 }
