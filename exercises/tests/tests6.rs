2c9a1ce306dbe36368dde115cfb2d2a7c40949fa
lengmuzhaxi <128277531+lengmuzhaxi@users.noreply.github.com>
2024-10-20 04:23:25 +0000

    solve iterators

--- exercises/tests/tests6.rs
+++ exercises/tests/tests6.rs
@@ -7,27 +7,25 @@
 // Execute `rustlings hint tests6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+///
+/// The `ptr` must contain an owned box of `Foo`.
 struct Foo {
     a: u128,
     b: Option<String>,
 }
-
-/// # Safety
-///
-/// The `ptr` must contain an owned box of `Foo`.
 unsafe fn raw_pointer_to_box(ptr: *mut Foo) -> Box<Foo> {
     // SAFETY: The `ptr` contains an owned box of `Foo` by contract. We
     // simply reconstruct the box from that pointer.
-    let mut ret: Box<Foo> = unsafe { ??? };
-    todo!("The rest of the code goes here")
+    let mut ret: Box<Foo> = unsafe { Box::from_raw(ptr) };
+    // Now we can modify the contents of the box safely.
+    ret.b = Some("hello".to_owned());
+    ret
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::time::Instant;
 
     #[test]
     fn test_success() {
@@ -43,3 +41,4 @@ mod tests {
         assert!(ret.b == Some("hello".to_owned()));
     }
 }
+
