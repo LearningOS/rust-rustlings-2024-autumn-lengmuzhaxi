5ec5cc156fae8ac4b5a77ca2a62f67cf50550a65
lengmuzhaxi <128277531+lengmuzhaxi@users.noreply.github.com>
2024-10-20 05:14:59 +0000

    solve clippys

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -9,23 +9,27 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        my_option.unwrap();
+
+    // 使用 is_some() 方法检查 Option
+    if my_option.is_some() {
+       // 这里可以处理 Some 的情况
     }
 
     let my_arr = &[
-        -1, -2, -3
-        -4, -5, -6
+        -1, -2, -3, // 添加缺少的逗号
+        -4, -5, -6,
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
+    // 创建空 Vec
+    let my_empty_vec: Vec<i32> = vec![];
     println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
-    // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    // 使用 std::mem::swap 交换值
+    std::mem::swap(&mut value_a, &mut value_b);
+    
     println!("value a: {}; value b: {}", value_a, value_b);
 }
+
